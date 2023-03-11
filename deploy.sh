acc9d5bae51785cbde2bc9aff468068948c88bb4
miquelvir <41950283+miquelvir@users.noreply.github.com>
2023-03-11 21:05:50 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 export $(grep -v '^#' .env | xargs)
 rsync -avz . avocadoArmchair@ecv-etic.upf.edu:/home/avocadoArmchair/p2 --exclude /frontend/config.js --exclude /node_modules --exclude .env.dev --exclude .gitignore --exclude /.git
 ssh avocadoArmchair@ecv-etic.upf.edu << EOF
-    cd /home/avocadoArmchair/p2
+    cd /home/avocadoArmchair/p3
     npm install
 
     mv ./frontend/config.prod.js ./frontend/config.js
